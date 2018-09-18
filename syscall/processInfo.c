fe6f27b0b5f0973985d3be6dd25f402b43283d1c
labredes <labredes@labredes.portoalegre.pucrsnet.br>
2018-09-18 08:30:50 -0300

    reestruturando o repositorio

---
+++ syscall/processInfo.c
@@ -0,0 +1,34 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+#include "processInfo.h"
+
+asmlikage ling sys_listProcessInfo(long pid, const char __user *buf, int size) {
+	struct task_struct *proces;
+	unsigned char kbuf[256];
+	int bufsz;
+	int ret;
+	
+	/* Find the process */
+	for_each_process(proces) {
+		if( (long)task_pid_nr(process) == pid){
+			/* Print the process info to the buffer */
+			snprintf(kbuf, sizeof(kbuf), "Process: %s\n PID_Number: %ld\n Process State: %ld\n Priority: %ld\n RT_Priority: %ld\n Static Priority: %ld Normal Priority: %ld\n", proces->comn, (long)task_pid_nr(proces), (long)proces->state, (long)proces->prio, (long)proces->rt_priority, (long)proces->static_prio, (long)proces-> normal_prio);
+			bufsz = strlen(kbuf)+1;
+			
+			/* User buffer is too small */
+			if(bufsz > size){
+				return -1;
+			}
+			
+			/* success */
+			ret = copy_to_user((void*)buf, (void*)kbuf, bufsz);
+			
+			return bufsz - ret;
+		}
+	}
+	
+	/* Process not found */
+	return -2;
+}
